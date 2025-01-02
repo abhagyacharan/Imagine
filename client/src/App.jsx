63c92b00b70e4884ef044b7bb5a227b9b37fa434
Bhagya Charan Aela <a.bhagyacharan@gmail.com>
2025-01-03 03:28:40 +0530

    Navbar added

---
+++ client/src/App.jsx
@@ -0,0 +1,22 @@
+import React from "react"
+import { Routes, Route } from "react-router-dom"
+
+import Home from './pages/Home'
+import Result from './pages/Result'
+import BuyCredit from './pages/BuyCredit'
+import Navbar from './components/Navbar'
+
+export default function App() {
+  return (
+    <div className="px-4 sm:px-10 md:px-14 lg:px-28 min-h-screen bg-gradient-to-b from-teal-100 to-violet-100">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/buy" element={<BuyCredit/>} />
+        <Route path="/result" element={<Result/>} />
+      </Routes>
+
+    </div>
+
+  )
+}
---
+++ client/src/pages/BuyCredit.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BuyCredit = () => {
+  return (
+    <div>Buy Credits</div>
+  )
+}
+
+export default BuyCredit
